24f9efb98dc9061e40050c540fac271bc80b6ad2
Fei Wang <FWang@loyalty.com>
2018-11-06 12:51:09 -0500

    Extened form.js to handle corrdinates and weather based on user's inputs

--- out/artifacts/rest_web_app_war_exploded/js/form.js
+++ out/artifacts/rest_web_app_war_exploded/js/form.js
@@ -43,6 +43,7 @@ $(document).ready(function() {
         });
     }
 
+// Function to save the user's form inputs as Json and send to Java User class
     function addUser() {
         console.log('addUser');
         $.ajax({
@@ -57,9 +58,10 @@ $(document).ready(function() {
                 var inputEmail = $("#emailAddress").val();
                 var inputCity = $("#city").val();
                 var inputCountry = $("#country").val();
-                $('#message').html("Hi " + inputFirstName + " " + inputLastName + ". "
+                $('#message').html("Hi, " + inputFirstName + " " + inputLastName + ". "
                     + "Your email address is " + inputEmail + ". " + "You are from "
                     + inputCity + ", " + inputCountry + ".");
+                cityToLatLng(inputCity, inputCountry)
             },
             error: function (jqXHR, textStatus, errorThrown) {
                 $('#message').html(textStatus);
@@ -79,6 +81,40 @@ $(document).ready(function() {
         });
     }
 
+// Function to return Latitude and Longitude from city and country by using Google Geocode API
+    function cityToLatLng(city, country) {
+        var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${city},+${country}&key=AIzaSyDh2B_89m0UN1CsHzeGaCYuRGQdD8u1H1I`
+        fetch(geocodeUrl).then(response => {
+            return response.json()
+        }).then(data => {
+            if(data.status == 'OK') {
+                var lat = data.results[0].geometry.location.lat
+                var lng = data.results[0].geometry.location.lng
+                $('#coordinates').html("Your city's coordinates: Latitude(" + lat +") Longitude(" + lng + ")")
+                getWeather(lat, lng)
+            }
+            else {
+                $('#coordinates').html("Sorry, we are not able to find your city. Please double check if your input is correct.")
+            }
+        }).catch(err => {console.log(err)})
+    }
+
+// Function to return current weather and temperature Latitude and Longitude by using Open Weather Map API
+    function getWeather(lat, lng) {
+        var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=172c823ecb710320d9fe119152e941a8`
+        fetch(weatherUrl).then(response => {
+            return response.json()
+        }).then(data => {
+            if(data.cod == '200') {
+                var weather = data.weather[0].main
+                var temperature = data.main.temp
+                $('#weather').html("Current weather is " + weather + ", temperature is " + temperature + " degree Celsius")
+            }
+            else {
+                $('#weather').html("Sorry, the current weather is not available.")
+            }
+        }).catch(err => {console.log(err)})
+    }
 /*
 // Helper function to serialize all the form fields into a JSON string
     function formToJSON() {
